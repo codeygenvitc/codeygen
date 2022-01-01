a5227cae0b88e1520c549e439621860ea1b229be
codeygenvitc <83817164+codeygenvitc@users.noreply.github.com>
2022-01-01 12:23:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-console.log("Prashant");
+//console.log("Prashant");
 
 // console.log(window.scrollTop());
 document.addEventListener("scroll", function() {
     var top = document.getElementById("mybt");
     if (window.scrollY > 600) {
-        console.log("scroll");
+        //console.log("scroll");
         top.classList.add("visible");
     } else {
         top.classList.remove("visible");
@@ -16,12 +16,12 @@ window.onload = function() {
     const cam = document.getElementById('cam');
     console.log(cam);
     cam.addEventListener('mouseenter', () => {
-        console.log('Prashant');
+        //console.log('Prashant');
         var but = document.getElementById("but");
         but.style.display = "unset";
     });
     cam.addEventListener('mouseleave', () => {
-        console.log('Prashant');
+        //console.log('Prashant');
         var but = document.getElementById("but");
         but.style.display = "none";
     });
